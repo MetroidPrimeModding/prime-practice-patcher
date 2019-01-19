54404ab8f5d6210963ea0e7396fc9037023c79bd
Pwootage <pwootage@gmail.com>
2019-01-18 20:43:12 -0700

    Start writing the new version (which will be native), so delete all the old code

--- index.js
+++
@@ -1,3 +0,0 @@
-require('ts-node').register();
-const {main} = require('./src/main');
-main();
--- src/commands/CommandBase.ts
+++
@@ -1,7 +0,0 @@
-import {Arguments, Argv, CommandModule} from "yargs";
-
-export interface CommandBase extends CommandModule {
-  builder(yargs: Argv): Argv;
-
-  handler(argv: Arguments): void;
-}
